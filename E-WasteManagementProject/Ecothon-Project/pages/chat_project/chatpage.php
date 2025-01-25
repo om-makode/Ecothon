0b5297a6569671009c64ffc4fcc19635c6a62a62
om-makode <ommakode2@gmail.com>
2025-01-25 23:00:32 +0530

    Ecothon

---
+++ E-WasteManagementProject/Ecothon-Project/pages/chat_project/chatpage.php
@@ -0,0 +1,106 @@
+<?php 
+	session_start();
+	if(isset($_SESSION['name']))
+	{
+		include "layouts/header2.php"; 
+		include "config.php"; 
+		
+		$sql="SELECT * FROM `chat`";
+
+		$query = mysqli_query($conn,$sql);
+?>
+<style>
+  h2{
+color:white;
+  }
+  label{
+color:white;
+  }
+  span{
+	  color:black;
+	  font-weight:bold;
+  }
+  .container {
+    margin-top: 3%;
+    width: 80%;
+    background: rgb(131,58,180);
+background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
+    padding-right:10%;
+    padding-left:10%;
+  }
+  .btn-primary {
+    background-color: #673AB7;
+	}
+	.display-chat{
+		height:300px;
+		background-color:#ffff;
+		margin-bottom:4%;
+		overflow:auto;
+		padding:20px;
+	}
+	.message{
+		background: rgb(93,222,218);
+background: radial-gradient(circle, rgba(93,222,218,1) 0%, rgba(58,134,223,1) 100%);
+		color: white;
+		border-radius: 5px;
+		padding: 5px;
+		margin-bottom: 3%;
+	}
+  </style>
+
+<div class="container">
+  <center><h2>Welcome <span style="color:black;"><?php echo $_SESSION['name']; ?> !</span></h2>
+	<label>Join the chat</label>
+  </center></br>
+  <div class="display-chat">
+<?php
+	if(mysqli_num_rows($query)>0)
+	{
+		while($row= mysqli_fetch_assoc($query))	
+		{
+?>
+		<div class="message">
+			<p>
+				<span><?php echo $row['name']; ?> :</span>
+				<?php echo $row['message']; ?>
+			</p>
+		</div>
+<?php
+		}
+	}
+	else
+	{
+?>
+<div class="message">
+			<p>
+				No previous chat available.
+			</p>
+</div>
+<?php
+	} 
+?>
+
+  </div>
+  <form class="form-horizontal" method="post" action="sendMessage.php">
+    <div class="form-group">
+      <div class="col-sm-10">          
+        <textarea name="msg" class="form-control" placeholder="Type your message here..."></textarea>
+      </div>
+	         
+      <div class="col-sm-2">
+        <button type="submit" class="btn btn-primary">Send</button>
+      </div>
+
+    </div>
+  </form>
+</div>
+
+</body>
+</html>
+<?php
+	}
+	else
+	{
+		header('location:index.php');
+	}
+?>
---
+++ E-WasteManagementProject/Ecothon-Project/pages/chat_project/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbHost ='localhost:3307';
+$dbUsername ='root';
+$dbPassword ='';
+$dbDatabase ='notes';
+$conn=mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbDatabase);
+
+?>
